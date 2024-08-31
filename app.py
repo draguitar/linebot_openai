3af60d1dbca884e8dd6c56c2af8f707c92068f70
Longer <draguitar@gmail.com>
2024-08-31 16:09:38 +0800

    fix bug

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
+api_key = os.getenv('OPENAI_API_KEY')
+api_base_url = os.getenv('OPENAI_API_BASE_URL')
 
 client = OpenAI(
     api_key=api_key,
